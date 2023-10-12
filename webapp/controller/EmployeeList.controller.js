e5952e6cd2299d4f37d0c58c00b5de33f57caf35
dawid cichon <dawid.cichon@capgemini.com>
2023-10-12 12:24:39 +0000

    3

--- webapp/controller/EmployeeList.controller.js
+++ webapp/controller/EmployeeList.controller.js
@@ -16,6 +16,7 @@ sap.ui.define([
                   .getRoute("EmployeeList")
             },
             onFire: function(){
+                /*
                 $.ajax({
                     url: "https://evilinsult.com/generate_insult.php?lang=en&type=json",
                     
@@ -28,7 +29,7 @@ sap.ui.define([
                         console.log(JSON.parse(oResponse));
                     }
                 });
-                /*
+
                 var oModel = new sap.ui.model.json.JSONModel();
                 oModel.loadData("https://evilinsult.com/generate_insult.php?lang=en&type=json").then(function () {
                             console.log(oModel.getData().insult);
@@ -36,6 +37,11 @@ sap.ui.define([
                         }
                     )
                 */
+                var oEmployee = oEvent.getSource().getBindingContext().getObject();
+                var sEmail = oEmployee.FirstName + "." + oEmployee.LastName + "@gmail.com"
+                var sSubject = "It is time to say goodbye...";
+                var sBody = "You are fired!"
+                sap.m.URLHelper.triggerEmail(sEmail, sSubject, sBody)
                 
             },
             onMotivate: function(oEvent){
